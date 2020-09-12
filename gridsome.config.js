057dcb45cd06a7df286c29f6ec5915cebf816b50
Chase Adams <realchaseadams@gmail.com>
2020-09-12 13:07:03 -0700

    fix tailwind config

--- gridsome.config.js
+++ gridsome.config.js
@@ -26,17 +26,12 @@ module.exports = {
   },
   plugins: [
     {
-      use: "gridsome-plugin-purgecss",
-      // default options, the following will be included if you don't provide anything
+      use: "gridsome-plugin-tailwindcss",
       options: {
-        content: [
-          "./src/**/*.vue",
-          "./src/**/*.js",
-          "./src/**/*.jsx",
-          "./src/**/*.pug",
-          "./src/**/*.md",
-        ],
-        defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+        tailwindConfig: "./tailwind.config.js",
+        presetEnvConfig: {},
+        shouldImport: true,
+        shouldTimeTravel: true,
       },
     },
     {
