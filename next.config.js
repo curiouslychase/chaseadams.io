ab1845a13767f93f6a7750d9e98bbdaa8ab568b2
Chase Adams <realchaseadams@gmail.com>
2021-02-23 20:58:18 -0800

    add redirect for most common programming case types

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
+const { redirect } = require("next/dist/next-server/server/api-utils");
+
 module.exports = {
   trailingSlash: true,
+  async redirects() {
+    return [
+      {
+        source: `/most-common-programming-case-types/`,
+        destination: `/posts/most-common-programming-case-types/`,
+        permanent: true,
+      },
+    ];
+  },
 };
