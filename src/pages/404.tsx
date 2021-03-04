acbb3501eea3962c52464d6486957d7270f9a4f8
Chase Adams <realchaseadams@gmail.com>
2021-03-04 06:19:49 -0800

    fix 404 page layout

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,16 +1,19 @@
+import MaxWidthWrapper from "~/components/MaxWidthWrapper";
 import DefaultLayout from "~/containers/layouts/Default";
 export default function Custom404() {
   return (
     <DefaultLayout>
-      <h1>Whoops! Page can't be found.</h1>
-      <p>
-        It looks like you're trying to access a page that can't be found. This
-        could be because the URL used to get here is incorrect.
-      </p>
-      <p>
-        Let me know you stumbled onto a broken link on{" "}
-        <a href="https://twitter.com/chaseadamsio">Twitter</a>!
-      </p>
+      <MaxWidthWrapper>
+        <h1>Whoops! Page can't be found.</h1>
+        <p>
+          It looks like you're trying to access a page that can't be found. This
+          could be because the URL used to get here is incorrect.
+        </p>
+        <p>
+          Let me know you stumbled onto a broken link on{" "}
+          <a href="https://twitter.com/chaseadamsio">Twitter</a>!
+        </p>
+      </MaxWidthWrapper>
     </DefaultLayout>
   );
 }
