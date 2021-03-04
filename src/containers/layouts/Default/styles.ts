39e0e1efe09b2837690ff34fb3775825b70db881
Chase Adams <realchaseadams@gmail.com>
2021-03-04 06:24:09 -0800

    add min-height for layout

--- src/containers/layouts/Default/styles.ts
+++ src/containers/layouts/Default/styles.ts
@@ -3,6 +3,10 @@ import styled from "styled-components";
 export const Container = styled.div`
   margin: 0 auto;
   padding: 1rem;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 
   @media (${({ theme }) => theme.responsive.phone}) {
     width: 100%;
