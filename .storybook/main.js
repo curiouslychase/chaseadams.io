9fa1dfbe99f3ca0f0346b3a167c1dd235dc984c3
Chase Adams <realchaseadams@gmail.com>
2021-03-21 05:29:05 -0700

    add MDX support to Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
 module.exports = {
   stories: ["../src/components/**/*.stories.@(ts|mdx)"], // glob for finding stories.
+  addons: [
+    "@storybook/addon-docs", // enables use of MDX to write stories
+  ],
 };
