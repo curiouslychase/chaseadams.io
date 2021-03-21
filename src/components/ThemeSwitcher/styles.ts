becaf7a94f805ffce6568c7b1da8f3b8cf04a13e
Chase Adams <realchaseadams@gmail.com>
2021-03-21 06:05:27 -0700

    fix button border, background and outline

--- src/components/ThemeSwitcher/styles.ts
+++ src/components/ThemeSwitcher/styles.ts
@@ -3,17 +3,19 @@ import styled from "styled-components";
 export const Button = styled.button`
   display: flex;
   justify-content: space-between;
-  padding-top: 0;
-  padding-right: 0;
-  padding-left: 0;
-  padding-bottom: 0;
+  padding-top: 0.25rem;
+  padding-right: 0.25rem;
+  padding-left: 0.25rem;
+  padding-bottom: 0.25rem;
   font-size: 1rem;
   align-items: center;
   color: var(--color-primary);
-  border-color: none;
-  background-color: none;
+  border-width: 0;
+  border-color: transparent;
+  background-color: transparent;
   cursor: pointer;
-  outline-color: none;
+  outline-width: 0;
+  outline-color: transparent;
 
   & span {
     padding-left: 0.5rem;
