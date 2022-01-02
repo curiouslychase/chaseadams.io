fbc8b1c4d6e66628f12dbaf6fc6dc4445679e940
Chase Adams <realchaseadams@gmail.com>
2022-01-02 06:02:03 -0700

    update PostSummary and postSummaries styling

--- src/components/modules/Post/PostSummaries/index.tsx
+++ src/components/modules/Post/PostSummaries/index.tsx
@@ -1,12 +1,20 @@
+import { Grid } from "~/components/core/Grid";
 import { PostSummary } from "~/components/modules/Post/PostSummary";
 import type { PostMeta } from "~/lib/posts/types";
 
 export const PostSummaries = ({ posts }: { posts: Array<PostMeta> }) => {
   return (
-    <div className="grid 2xl:grid-cols-3 md:grid-cols-2 gap-6">
+    <Grid
+      css={{
+        gap: "$6",
+        gridTemplateColumns: `repeat(1, minmax(0, 1fr))`,
+        "@md": { gridTemplateColumns: `repeat(2, minmax(0, 1fr))` },
+        "@lg": { gridTemplateColumns: `repeat(3, minmax(0, 1fr))` },
+      }}
+    >
       {posts.map((post) => (
         <PostSummary key={post.slug} post={post} />
       ))}
-    </div>
+    </Grid>
   );
 };
