97ad860cb830c25b9b4207a76f42cc7a72e1692e
Chase Adams <realchaseadams@gmail.com>
2022-01-02 06:04:00 -0700

    rm DarkModeContainer component

--- src/components/shared/DarkModeContainer.tsx
+++
@@ -1,9 +0,0 @@
-import type { FC } from "react";
-
-import { useThemeMode } from "~/contexts/ThemeContext";
-
-export const DarkModeContainer: FC = ({ children }) => {
-  const { colorMode } = useThemeMode();
-
-  return <div className={colorMode}>{children}</div>;
-};
